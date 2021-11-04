7d0cb85c9eabc41e6f7f27e50bb72550748f42cc
CristianRodrigoGuarachiIbanez <41841604+CristianRodrigoGuarachiIbanez@users.noreply.github.com>
2021-11-04 17:24:42 +0100

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,41 +2,62 @@
 #include <stdio.h>
 #include   <stdlib.h>
 #include <iostream>
-//https://stackoverflow.com/questions/29758662/c-conversion-from-numpy-array-to-mat-opencv
-int printInt(****int arr{
-    for(int i =0; i<5;i**){
+//https://stackoverflow.com/questions/29758662/c-conversion-from-numpy-array-to-mat-opencv#
+void populate(int*****arr){
+    for(int i =0; i<5;i++){
+       for(int o=0;o<2;o++){
        for(int j =0; j<7; j++){
          for(int k=0;k<10;k++){
            for(int n=0;n<8;n++){
-             std::cout<<arr[i][j][k][n]<<std::endl;
+            arr[i][o][j][k][n] =2;
              }
            }
          }
        }
+    }
+}
+
+void printInt(int*****arr){
+    int counter = 0;
+    for(int i =0; i<5;i++){
+       for(int o=0;o<2;o++){
+       for(int j =0; j<7; j++){
+         for(int k=0;k<10;k++){
+           for(int n=0;n<8;n++){
+            counter++;
+            //std::cout<<counter<<std::endl;
+            std::cout<<counter<< ":" <<arr[i][o][j][k][n]<<std::endl;
+             }
+           }
+         }
+       }
+    }
 }
 
 int main(){
-  int w = 5, x = 7, y = 10, z = 8;
-  int ****array;
+  int w = 5, im=2, x = 7, y = 10, z = 8;
+  int *****array;
   int i, j, k;
 
-  array = malloc(sizeof(double ***) * w);
-
+  array = (int *****)malloc(sizeof(double ****) * w);
+ 
   for(i = 0;i < w;++i)
   {
-    array[i] = malloc(sizeof(double **) * x);
+    array[i] = (int****)malloc(sizeof(double***)*im);  
+    for(int n=0;n<im;n++){  
+         array[i][n] = (int***)malloc(sizeof(double **) * x);
 
     for(j = 0;j < x;++j)
     {
-      array[i][j] = malloc(sizeof(double *) * y);
+      array[i][n][j] = (int**)malloc(sizeof(double *) * y);
 
       for(k = 0;k < y;++k)
-        array[i][j][k] = malloc(sizeof(double) * z);
+        array[i][n][j][k] = (int*)malloc(sizeof(double) * z);
        }
     }
-
+  }
+    populate(array);
     printInt(array);
-
 
     return 0;
 }
